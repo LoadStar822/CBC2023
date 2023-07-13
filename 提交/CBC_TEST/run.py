f278490625b73cf1b48b4eab4056e896849c322f
LoadStar <992247533@qq.com>
2023-07-13 20:33:41 +0800

    test

--- 提交/CBC_TEST/run.py
+++ 提交/CBC_TEST/run.py
@@ -64,53 +64,58 @@ def getEmbedding(sequence, model, tokenizer):
     return emb
 
 
-for fn in fileList:
-    structId = os.path.basename(fn)
+try:
+    for fn in fileList:
+        structId = os.path.basename(fn)
 
-    with open(fn, 'r') as fileHandle:
-        scop = json.load(fileHandle)
-    model = scop['model']
-    size = len(model)
-    dist2, coordinates = calDist2(model)
-    frag = np.array([isFrag(model, dist2, i) for i in range(size)], dtype=np.bool_)
-    frag2 = np.zeros([size, size], dtype=np.bool_)
+        with open(fn, 'r') as fileHandle:
+            scop = json.load(fileHandle)
+        model = scop['model']
+        size = len(model)
+        dist2, coordinates = calDist2(model)
+        frag = np.array([isFrag(model, dist2, i) for i in range(size)], dtype=np.bool_)
+        frag2 = np.zeros([size, size], dtype=np.bool_)
 
-    for index0, res0 in model.items():
-        index0 = int(index0) - 1
+        for index0, res0 in model.items():
+            index0 = int(index0) - 1
 
-        if float(res0['nho0e']) <= hbondmax:
-            index1 = index0 + int(res0['nho0p'])
-            if isFrag2(dist2, frag, index0, index1):
-                frag2[index0, index1] = True
+            if float(res0['nho0e']) <= hbondmax:
+                index1 = index0 + int(res0['nho0p'])
+                if isFrag2(dist2, frag, index0, index1):
+                    frag2[index0, index1] = True
 
-        if float(res0['nho1e']) <= hbondmax:
-            index2 = index0 + int(res0['nho1p'])
-            if isFrag2(dist2, frag, index0, index2):
-                frag2[index0, index2] = True
-    hbond = [buildContact(model, i, j) for i, j in zip(*np.where(frag2))]
-    sequence = scop['sequence']
-    emd = getEmbedding(sequence, modelEncode, tokenizer)
-    predictions.append(dict(coord=coordinates, hbond=hbond, emd=emd))
-    strId.append(structId[:-5])
-    os.remove(fn)
+            if float(res0['nho1e']) <= hbondmax:
+                index2 = index0 + int(res0['nho1p'])
+                if isFrag2(dist2, frag, index0, index2):
+                    frag2[index0, index2] = True
+        hbond = [buildContact(model, i, j) for i, j in zip(*np.where(frag2))]
+        sequence = scop['sequence']
+        emd = getEmbedding(sequence, modelEncode, tokenizer)
+        predictions.append(dict(coord=coordinates, hbond=hbond, emd=emd))
+        strId.append(structId[:-5])
+        os.remove(fn)
+except Exception as e:
+    print(f"An error occurred: {e}")
+try:
+    modelFileName = 'ESMC/model_sav/modelnew_model_augmentation-seqid3-run1.pth'
+    pt.cuda.set_device(int(gpuDevice))
+    modelAugment = new_model_augmentation(depth=3, width=1024, multitask=True).cuda()
+    modelAugment.load_state_dict(pt.load(modelFileName))
+    modelAugment.eval()
 
-modelFileName = 'ESMC/model_sav/modelnew_model_augmentation-seqid3-run1.pth'
-pt.cuda.set_device(int(gpuDevice))
-modelAugment = new_model_augmentation(depth=3, width=1024, multitask=True).cuda()
-modelAugment.load_state_dict(pt.load(modelFileName))
-modelAugment.eval()
-
-predictionLoader = iterPredictBond(predictions, 1)
-dataSize, resultSize = len(predictions), 0
-for x, m, e in predictionLoader:
-    x = Variable(x).cuda()
-    m = Variable(m).cuda()
-    e = Variable(e).cuda()
-    _, _, yy2, _ = modelAugment(x, m, e)
-    yy2Idx = pt.argmax(yy2, dim=1)
-    yy2Idx = yy2Idx.cpu().numpy()[0].tolist()
-    yy2Lab = idx2lab[yy2Idx]
-    givenId = strId[resultSize]
-    print(yy2Lab)
-    resultSize += x.size(0)
-    if resultSize >= dataSize: break
+    predictionLoader = iterPredictBond(predictions, 1)
+    dataSize, resultSize = len(predictions), 0
+    for x, m, e in predictionLoader:
+        x = Variable(x).cuda()
+        m = Variable(m).cuda()
+        e = Variable(e).cuda()
+        _, _, yy2, _ = modelAugment(x, m, e)
+        yy2Idx = pt.argmax(yy2, dim=1)
+        yy2Idx = yy2Idx.cpu().numpy()[0].tolist()
+        yy2Lab = idx2lab[yy2Idx]
+        givenId = strId[resultSize]
+        print(yy2Lab)
+        resultSize += x.size(0)
+        if resultSize >= dataSize: break
+except Exception as e:
+    print(f"An error occurred: {e}")
